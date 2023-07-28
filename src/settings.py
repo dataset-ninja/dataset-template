724e66ead5dc0198433eb879b3917188b2c4d228
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-07-28 17:09:22 +0600

    rm PAPER List[str]

--- src/settings.py
+++ src/settings.py
@@ -40,7 +40,7 @@ DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
-PAPER: Optional[Union[str, List[str]]] = None
+PAPER: Optional[str] = None
 CITATION_URL: Optional[str] = None
 AUTHORS: Optional[List[str]] = None
 
